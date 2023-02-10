4aa0a7f4716b58968ea7fa8ddc315f9b47877eaa
Shahman Magomadov <magomadovshahman@gmail.com>
2023-02-10 17:50:17 +0300

    add get and post metods

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,13 +1,47 @@
 package org.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.model.User;
+import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/api/users")
 public class MainController {
 
-	@GetMapping("/")
-	public String greeting() {
-		return "index";
+	final
+	UserService userService;
+
+	public MainController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUser(@PathVariable("id") Long userId) {
+		if (userId == null)
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+		User user = userService.getById(userId);
+
+		if (user == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
+	@PostMapping()
+	public ResponseEntity<User> saveUser(@RequestBody @Valid User user) {
+		HttpHeaders headers = new HttpHeaders();
+
+		if (user == null)
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+		this.userService.save(user);
+		return new ResponseEntity<>(user, headers, HttpStatus.CREATED);
 	}
 }
