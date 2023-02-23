4761df7475666454688a3ea126aea6993d314614
Shahman Magomadov <magomadovshahman@gmail.com>
2023-02-23 20:22:39 +0300

    mod db

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -1,12 +1,40 @@
 package org.example.config;
 
-public class WebSecurityConfig {
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//				.antMatchers("/user/**").hasRole("USER")
-//				.antMatchers("/admin/**").hasRole("ADMIN")
-//				.and()
-//				.formLogin();
-//	}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	private final DataSource dataSource;
+
+	public WebSecurityConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+				.dataSource(dataSource)
+				.usersByUsernameQuery("select username,password,enabled "
+						+ "from users where username=?")
+				.authoritiesByUsernameQuery("select username, authority "
+						+ "from authorities where username=?")
+				.passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+					.antMatchers("/admin/**").hasRole("ADMIN")
+					.antMatchers("/teacher/**").hasRole("TEACHER")
+					.antMatchers("/student/**").hasRole("STUDENT")
+				.and()
+					.formLogin()
+				.and()
+					.logout()
+					.logoutSuccessUrl("/");
+	}
 }
--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,4 +1,4 @@
-INSERT INTO Users (name, email, password)
+INSERT INTO Users (name, email, password, )
 VALUES ('User', 'User@yandex.ru', 'password'),
        ('Admin', 'admin@gmail.com', 'admin'),
        ('Guest', 'guest@gmail.com', 'guest');
