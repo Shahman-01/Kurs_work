05ea485d6b9af20f176987eb83004b6483cb5aeb
Shahman Magomadov <magomadovshahman@gmail.com>
2023-02-23 20:05:52 +0300

    comment

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -1,12 +1,12 @@
 package org.example.config;
 
 public class WebSecurityConfig {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/user/**").hasRole("USER")
-				.antMatchers("/admin/**").hasRole("ADMIN")
-				.and()
-				.formLogin();
-	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//				.antMatchers("/user/**").hasRole("USER")
+//				.antMatchers("/admin/**").hasRole("ADMIN")
+//				.and()
+//				.formLogin();
+//	}
 }
